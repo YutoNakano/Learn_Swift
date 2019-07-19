334b4c085f80f151f5930929762f90663b944651
YutoNakano <yuto.44.bb@gmail.com>
2019-07-19 09:57:03 +0900

    add: practice storyboardreference

---
+++ section11/section11/ViewController 2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  section11
+//
+//  Created by 中野湧仁 on 2019/07/17.
+//  Copyright © 2019 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ section11/section11/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  section11
+//
+//  Created by 中野湧仁 on 2019/07/17.
+//  Copyright © 2019 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func goToTre(_ sender: Any) {
+        
+        //次画面がUINavigationControllerの場合
+        
+
+        
+        
+        //Storyboard名とidentifierで遷移
+//        var sb: UIStoryboard = UIStoryboard(name: "Next", bundle: nil)
+//        var nv = sb.instantiateViewController(withIdentifier: "treePage")
+//        self.present(nv, animated: true, completion: nil)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let navVC = segue.destination
+//        let nextVC = navVC.
+    }
+
